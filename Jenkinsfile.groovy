a0c3d6b5da0ab94788b5b6c13e40783b79a11e45
Israel Fermin Montilla <ferminster@gmail.com>
2018-03-20 15:11:19 +0400

    Simplifies the pipeline

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,4 @@
 #!/usr/bin/env groovy
-@Library('PipelineLibrary')
-import com.project.LogUtils
 
 pipeline {
     stages {
@@ -9,26 +7,5 @@ pipeline {
                 echo "This is just a test"
             }
         }
-        stage('Stage 2') {
-            steps {
-                testFunction()
-            }
-        }
-        stage('Test library function') {
-            steps {
-                sayHello()
-            }
-        }
-        stage('Test library class') {
-            steps {
-                script {
-                    echo LogUtils.log('This is a log message')
-                }
-            }
-        }
     }
-}
-
-def testFunction() {
-    echo "${env.VarOne} ${env.VarTwo}"
 }
