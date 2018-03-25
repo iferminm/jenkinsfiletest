a9b4f0a8402a3fe7e309ffe74aac1f7c6e208d8d
Israel Fermin Montilla <ferminster@gmail.com>
2018-03-25 13:39:18 +0400

    Surrounds everything in a try-catch clause

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,17 +1,21 @@
 #!/usr/bin/env groovy
 
 def start() {
-    stage('test') {
-        println "REMOTEANDO ${env.projectType}" 
-    }
-    stage('test library function') {
-        sayHello()
-    }
-    stage('Publishing to slack') {
-        slackLogger.info("This is info")
-        slackLogger.warning("This is warning")
-        slackLogger.failure("This is failure")
-        slackLogger.success("This is success")
+    try {
+        stage('test') {
+            println "REMOTEANDO ${env.PROJECT_TYPE}" 
+        }
+        stage('test library function') {
+            sayHello()
+        }
+        stage('Publishing to slack') {
+            slackLogger.info("This is info")
+            slackLogger.warning("This is warning")
+            slackLogger.failure("This is failure")
+            slackLogger.success("This is success")
+        }
+    catch(err) {
+        throw err
     }
 }
 
