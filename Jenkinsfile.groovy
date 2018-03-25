deb222cedc21e3a601e76180b1d9e382a9ddb20c
Israel Fermin Montilla <ferminster@gmail.com>
2018-03-25 13:43:28 +0400

    Back to a working version

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,23 +1,18 @@
 #!/usr/bin/env groovy
 
 def start() {
-    script {
-    try {
-        stage('test') {
-            println "REMOTEANDO ${env.PROJECT_TYPE}" 
-        }
-        stage('test library function') {
-            sayHello()
-        }
-        stage('Publishing to slack') {
-            slackLogger.info("This is info")
-            slackLogger.warning("This is warning")
-            slackLogger.failure("This is failure")
-            slackLogger.success("This is success")
-        }
-    catch(err) {
-        throw err
-    }}
+    stage('test') {
+        println "REMOTEANDO ${env.PROJECT_TYPE}" 
+    }
+    stage('test library function') {
+        sayHello()
+    }
+    stage('Publishing to slack') {
+        slackLogger.info("This is info")
+        slackLogger.warning("This is warning")
+        slackLogger.failure("This is failure")
+        slackLogger.success("This is success")
+    }
 }
 
 
